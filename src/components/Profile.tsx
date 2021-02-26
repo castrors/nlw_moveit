bab4aad1efc3255019cf1b0b4938313f8b89342d
Rodrigo Castro <rodrigo.castro@avira.com>
2021-02-26 12:19:36 +0100

    Finishes the day 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,18 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengeContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/castrors.png" alt="Rodrigo Castro" />
             <div>
                 <strong>Rodrigo Castro</strong>
                 <p>
-                    <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
                 </p>
             </div>
         </div>
